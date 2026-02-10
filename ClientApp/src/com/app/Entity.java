7e1fe6c6a73397b824b6f4aca13983333f362452
HP <HP@DESKTOP-1O64LH0.mshome.net>
2026-02-10 18:38:55 +0530

    commit by dev3 Sanika

--- ClientApp/src/com/app/Entity.java
+++ ClientApp/src/com/app/Entity.java
@@ -5,14 +5,8 @@ public class Entity {
 	public void m1() {
 		
 	}
-<<<<<<< HEAD
 	
 	public void m3() {
-=======
-
-	public void m2() {
->>>>>>> branch 'main' of https://github.com/rohit216-ui/ClientApp.git
 		
 	}
 }
-
